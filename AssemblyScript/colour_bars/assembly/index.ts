9efcd46182bf28c0f7c41ef8e382ab31edf819a8
Ian Wigley <ian.wigley@outlook.com>
2023-11-29 15:07:16 +0000

    Add movement

--- AssemblyScript/colour_bars/assembly/index.ts
+++ AssemblyScript/colour_bars/assembly/index.ts
@@ -1,25 +1,34 @@
-// Grow memory by 37 pages (128Kb)
-let pointer = memory.grow(37);
-memory.fill(pointer, 255, 37);
+// Grow memory by 30 pages (1 page = 64KB)
+memory.grow(30);
 
 const numberOfValues = 31;
-const colours = new Uint8Array(numberOfValues);
-colours.set([
+const greyColourBar = new Uint8Array(numberOfValues);
+greyColourBar.set([
   0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff,
   0xee, 0xdd, 0xcc, 0xbb, 0xaa, 0x99, 0x88, 0x77, 0x66, 0x55, 0x44, 0x33, 0x22, 0x11, 0x00
 ]);
 
+let startY: u32 = 16;
+const canvasWidth: u32 = 600;
+const numOfBytes: u32 = 4;
+
 export function go(): void {
-  let start: u32 = 50 * 600 * 4;
+  let start: u32 = startY * canvasWidth * numOfBytes;
   let x: u32 = 0;
   for (let y: i32 = 0; y < numberOfValues; y++) {
-    for (x = 0; x < 600 * 4; x += 4) {
-      store<u8>(start + x, colours[y]);
-      store<u8>(start + x + 1, colours[y]);
-      store<u8>(start + x + 2, colours[y]);
+    for (x = 0; x < canvasWidth * numOfBytes; x += numOfBytes) {
+      store<u8>(start + x, greyColourBar[y]);
+      store<u8>(start + x + 1, greyColourBar[y]);
+      store<u8>(start + x + 2, greyColourBar[y]);
       store<u8>(start + x + 3, 255);
     }
     start += x;
+  }
+  if (startY < 640) {
+    startY += 1;
+  }
+  else {
+    startY = 16;
   }
 }
 
